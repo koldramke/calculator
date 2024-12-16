4c1eebf038c7ba0fd39738ff75ddf064bc5238c7
koldramke <jose_egrl@hotmail.com>
2024-12-16 10:26:22 +0000

    creation of data response - dto

--- src/main/java/com/api/calculator/controller/CalculatorController.java
+++ src/main/java/com/api/calculator/controller/CalculatorController.java
@@ -17,22 +17,22 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping("/add")
+    @GetMapping("/a")
     public BigDecimal add(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         return calculatorService.add(a, b);
     }
 
-    @GetMapping("/subtract")
+    @GetMapping("/s")
     public BigDecimal subtract(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         return calculatorService.subtract(a, b);
     }
 
-    @GetMapping("/multiply")
+    @GetMapping("/m")
     public BigDecimal multiply(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         return calculatorService.multiply(a, b);
     }
 
-    @GetMapping("/divide")
+    @GetMapping("/d")
     public BigDecimal divide(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
         return calculatorService.divide(a, b);
     }
